ead729e2035043685d30b84b06cb7e3bbf9be251
Arnold <arnold.zephir@gmail.com>
2015-12-05 14:15:03 +0100

    use env variable

--- tweetq/tweetbot.js
+++ tweetq/tweetbot.js
@@ -1,10 +1,10 @@
 var Twitter = require('twitter');
  
  var client = new Twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
 
