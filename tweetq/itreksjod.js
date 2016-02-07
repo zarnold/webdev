10d9076152c61c844f6d226d745f24dee3bc8eac
ZEPHIR, Arnold <arzephir@bouyguestelecom.fr>
2016-02-07 15:43:21 +0100

    longer interval

--- tweetq/itreksjod.js
+++ tweetq/itreksjod.js
@@ -16,10 +16,10 @@ var DunjonMaster = function (name)
 
   this.name = name;
   this.client = new Twitter({
-	consumer_key: process.env.TWITTER_CONSUMER_KEY,
-	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-	access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+	consumer_key: '',
+	consumer_secret: '',
+	access_token_key: '',
+	access_token_secret:'' 
   });
 
   // Bind or loose ref
