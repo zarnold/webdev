5d12b8708e5f530cbec26aa8f37721dd383437b1
zarnold <zarnold@9online.fr>
2016-04-17 11:27:17 +0200

    random generator and ui

--- randomsheet/js/rsheet.js
+++ randomsheet/js/rsheet.js
@@ -1,16 +1,39 @@
 angular.module('rsheetApp', [])
   .controller('rsheetController', function($scope) {
+// Pour tester le setup uniquement
     $scope.dummyVar = 3;
-
-    $scope.persos=[
-      { 'name':'Alienor','sexe':'F'},
-      { 'name':'Luis','sexe':'M'},
-      { 'name':'Annette','sexe':'F'},
-      { 'name':'Ibrahim','sexe':'M'}
-    ];
 
     $scope.changeVar = function(){
 	$scope.dummyVar=8;
     };
- 
+
+// rsheet classe
+    //local random generator
+    var r=new Random(190779);
+    $scope.nbPersos=6;
+
+    $scope.genPerso = function()
+    {
+      var weight = Math.floor(r.normal(70,20));
+      var size = Math.floor(r.normal(170,50));
+
+      var p = { 'weight':weight,'size' : size}; 
+
+      return p;
+    };
+
+    $scope.genRoaster = function ()
+    {
+      var t=[];
+      for (i=0;i<$scope.nbPersos; i++)
+	t.push($scope.genPerso());
+
+      return t;
+    };
+
+    $scope.regen = function(){
+      $scope.persos=$scope.genRoaster();
+    };
+  
+    $scope.regen(); 
   });
