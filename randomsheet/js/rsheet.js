0b86944794382579d3849693cfa5aaf9c1de5920
zarnold <zarnold@9online.fr>
2016-04-17 15:19:58 +0200

    UX seems fine

--- randomsheet/js/rsheet.js
+++ randomsheet/js/rsheet.js
@@ -10,29 +10,48 @@ angular.module('rsheetApp', [])
 // rsheet classe
     //local random generator
     var r=new Random(190779);
-    $scope.nbPersos=2;
-
-    $scope.genPerso = function()
+    $scope.nbPersos=5;
+    function genPerso()
     {
       var weight = Math.floor(r.normal(70,20));
       var size = Math.floor(r.normal(170,50));
+      var name = "Omnomnom";
+      var sex = Math.random() > 0.5 ? 'F' :'M';
 
-      var p = { 'weight':weight,'size' : size}; 
+      var p = { 
+	'sex' : sex,
+	'name' : name,
+	'weight':weight,
+	'size' : size
+      }; 
 
       return p;
     };
 
-    $scope.genRoaster = function ()
+    function genRoaster()
     {
       var t=[];
       for (i=0;i<$scope.nbPersos; i++)
-	t.push($scope.genPerso());
+	t.push(genPerso());
 
       return t;
     };
 
+    $scope.incPerso = function(){
+      $scope.nbPersos++;
+      $scope.persos.push(genPerso());
+    };
+
+    $scope.decPerso = function(){
+      if ( $scope.nbPersos > 2 ) 
+      {
+	$scope.nbPersos--;
+	$scope.persos.pop();
+      }
+    };
+
     $scope.regen = function(){
-      $scope.persos=$scope.genRoaster();
+      $scope.persos=genRoaster();
     };
   
     $scope.regen(); 
